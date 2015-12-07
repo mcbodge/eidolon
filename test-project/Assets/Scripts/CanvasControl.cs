5e379d19440718e3a5675b7d6f221ccd18403448
mcbodge <manuel.grillo@mail.polimi.it>
2015-12-08 00:55:05 +0100

    Be aware of the cylinder... D:

---
+++ test-project/Assets/Scripts/CanvasControl.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class CanvasControl : MonoBehaviour
+{
+
+    void Start()
+    {
+        SetDefaultMessage();
+    }
+
+    //TODO improve
+    public Text TestText;
+
+    public void SetTestText(string newText)
+    {
+        TestText.text = newText;
+    }
+
+    public void SetCaughtMessage ()
+    {
+        TestText.text = "!!!";
+    }
+
+    public void SetDefaultMessage()
+    {
+        TestText.text = "-";
+    }
+
+}
