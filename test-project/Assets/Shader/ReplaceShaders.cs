64d1a44491c08b8e02afbbc0cd9f49eb60290b78
Domenico Iezzi <domenico.iezzi.201@gmail.com>
2015-12-12 19:42:45 +0100

    Huge cleanup of materials, added Shader
    Removed Materials downloaded from unity asset store and useless materials.
    Added shader toon-like

---
+++ test-project/Assets/Shader/ReplaceShaders.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ReplaceShaders : MonoBehaviour {
+
+	public static Shader myShader;
+
+	void Start () {
+
+		myShader = Shader.Find( "Toon" );
+
+	 	GameObject[] allObjects = UnityEngine.Object.FindObjectsOfType<GameObject>();
+		foreach(GameObject myObj in allObjects) {
+			if (myObj.GetComponent<Renderer>()) {
+				Material[] allMaterials = myObj.GetComponent<Renderer>().materials;
+				foreach(Material mat in allMaterials)
+					mat.shader = myShader;
+			}
+		}
+	}
+	
+	void Update () {
+	
+	}
+}
