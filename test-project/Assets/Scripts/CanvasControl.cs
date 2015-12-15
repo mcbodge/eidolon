f321fdcdc1e1ca702e32664d4efa4a4e8f670701
Domenico Iezzi <domenico.iezzi.201@gmail.com>
2015-12-15 12:28:35 +0100

    Lot of fixes, added tutorial

--- test-project/Assets/Scripts/CanvasControl.cs
+++ test-project/Assets/Scripts/CanvasControl.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine.UI;
 
 public class CanvasControl : MonoBehaviour
@@ -7,8 +8,11 @@ public class CanvasControl : MonoBehaviour
 
     public Text TopLeftMessage;
 	public Text TopRightMessage;
+    public Image ImageBox;
+    public List<Sprite> Images;
 
-    private int count;
+    private int imageShown;
+    private bool tutorialEnabled;
     private static CanvasControl canvasControlReference;
 
     public CanvasControl () 
@@ -21,7 +25,20 @@ public class CanvasControl : MonoBehaviour
     void Start()
     {
         SetDefaultMessage();
-        count = 0;
+        imageShown = 0;
+        tutorialEnabled = false;
+        ImageBox.enabled = tutorialEnabled;
+    }
+
+    void Update()
+    {
+        if (tutorialEnabled)
+        {
+            if (Input.GetKeyDown(KeyCode.Space)) {
+                AddImageToCanvas();
+            }
+        }
+
     }
 
     public static CanvasControl GetManager()
@@ -46,7 +63,6 @@ public class CanvasControl : MonoBehaviour
 
     public void SetCaughtMessage ()
     {
-        count++;
         TopLeftMessage.text = string.Format("!!!");
     }
 
@@ -54,5 +70,28 @@ public class CanvasControl : MonoBehaviour
     {
         TopLeftMessage.text = "";
     }
+
+    public void AddImageToCanvas()
+    {
+        if (imageShown < Images.Count)
+        {
+            ImageBox.sprite = Images[imageShown];
+            imageShown++;
+        } else
+        {
+            ImageBox.enabled = false;
+            tutorialEnabled = false;
+            imageShown = 0;
+        }
+            
+    }
+
+    public void TutorialEnable()
+    {
+        ImageBox.enabled = true;
+        tutorialEnabled = true;
+        AddImageToCanvas();
+    }
+    
 
 }
