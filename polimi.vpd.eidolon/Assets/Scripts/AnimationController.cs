187cc9a349cc3728ed26839d7ab1a18b594043a2
Domenico Iezzi <domenico.iezzi.201@gmail.com>
2016-01-07 11:47:21 +0100

    Added animation controller script to be used with new models

---
+++ polimi.vpd.eidolon/Assets/Scripts/AnimationController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimationController : MonoBehaviour {
+
+    private Animator animator;
+
+	// Use this for initialization
+	void Start () {
+        animator = GetComponent<Animator>();
+	}
+
+    public void StartColdAnimation()
+    {
+        animator.SetInteger("AnimControl", 1);
+    }
+
+    public void StartShowerAnimation()
+    {
+        animator.SetInteger("AnimControl", 2);
+    }
+
+    public void StartBeerAnimation()
+    {
+        animator.SetInteger("AnimControl", 3);
+    }
+
+    public void StopAnimation()
+    {
+        animator.SetInteger("AnimControl", 0);
+    }
+}
