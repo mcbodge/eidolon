de406d21519ace7a71d8fe287088e88343748623
mcbodge <manuel.grillo@mail.polimi.it>
2016-01-16 23:07:30 +0100

    Added BlurControl. Added Effect Standard Assets.

---
+++ polimi.vpd.eidolon/Assets/Scripts/BlurControl.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityStandardAssets.ImageEffects;
+
+public class BlurControl : MonoBehaviour
+{
+
+    public Camera MainCamera;
+    public GameObject Target;
+
+    private float multiplyingFactor;
+    private Vector3 targetPosition;
+
+    // Use this for initialization
+    void Start()
+    {
+        targetPosition = Target.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name.Equals("Eidolon"))
+        {
+            //multiplyingFactor = Vector3.Distance(source.transform.position, targetPosition);
+            MainCamera.GetComponent<BlurOptimized>().enabled = true;
+        }
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        GameObject source = other.gameObject;
+        if (source.name.Equals("Eidolon"))
+        {
+            var distance = Vector3.Distance(source.transform.position, targetPosition);
+            //MainCamera.GetComponent<BlurOptimized>().blurSize = true;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.name.Equals("Eidolon"))
+            MainCamera.GetComponent<BlurOptimized>().enabled = false;
+    }
+}
