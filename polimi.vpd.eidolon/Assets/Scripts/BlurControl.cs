adfccab3d68c0c68f813c141949bc63b3d7411ce
Domenico Iezzi <domenico.iezzi.201@gmail.com>
2016-01-20 22:13:47 +0100

    Big cleanup and ordering of folders

--- polimi.vpd.eidolon/Assets/Scripts/BlurControl.cs
+++ polimi.vpd.eidolon/Assets/Scripts/BlurControl.cs
@@ -1,50 +1,50 @@
-﻿using UnityEngine;
-using System.Collections;
-using UnityStandardAssets.ImageEffects;
+﻿//using UnityEngine;
+//using System.Collections;
+//using UnityStandardAssets.ImageEffects;
 
-public class BlurControl : MonoBehaviour
-{
+//public class BlurControl : MonoBehaviour
+//{
 
-    public Camera MainCamera;
-    public GameObject Target;
+//    public Camera MainCamera;
+//    public GameObject Target;
 
-    private float multiplyingFactor;
-    private Vector3 targetPosition;
+//    private float multiplyingFactor;
+//    private Vector3 targetPosition;
 
-    // Use this for initialization
-    void Start()
-    {
-        targetPosition = Target.transform.position;
-    }
+//    // Use this for initialization
+//    void Start()
+//    {
+//        targetPosition = Target.transform.position;
+//    }
 
-    // Update is called once per frame
-    void Update()
-    {
+//    // Update is called once per frame
+//    void Update()
+//    {
 
-    }
+//    }
 
-    void OnTriggerEnter(Collider other)
-    {
-        GameObject source = other.gameObject;
-        if (source.name.Equals("Eidolon"))
-        {
-            multiplyingFactor = 10f / Vector3.Distance(source.transform.position, targetPosition);
-            MainCamera.GetComponent<BlurOptimized>().enabled = true;
-        }
-    }
+//    void OnTriggerEnter(Collider other)
+//    {
+//        GameObject source = other.gameObject;
+//        if (source.name.Equals("Eidolon"))
+//        {
+//            multiplyingFactor = 10f / Vector3.Distance(source.transform.position, targetPosition);
+//            MainCamera.GetComponent<BlurOptimized>().enabled = true;
+//        }
+//    }
 
-    void OnTriggerStay(Collider other)
-    {
-        GameObject source = other.gameObject;
-        if (source.name.Equals("Eidolon"))
-        {
-            MainCamera.GetComponent<BlurOptimized>().blurSize = 10 - multiplyingFactor * Vector3.Distance(source.transform.position, targetPosition);
-        }
-    }
+//    void OnTriggerStay(Collider other)
+//    {
+//        GameObject source = other.gameObject;
+//        if (source.name.Equals("Eidolon"))
+//        {
+//            MainCamera.GetComponent<BlurOptimized>().blurSize = 10 - multiplyingFactor * Vector3.Distance(source.transform.position, targetPosition);
+//        }
+//    }
 
-    void OnTriggerExit(Collider other)
-    {
-        if (other.gameObject.name.Equals("Eidolon"))
-            MainCamera.GetComponent<BlurOptimized>().enabled = false;
-    }
-}
+//    void OnTriggerExit(Collider other)
+//    {
+//        if (other.gameObject.name.Equals("Eidolon"))
+//            MainCamera.GetComponent<BlurOptimized>().enabled = false;
+//    }
+//}
