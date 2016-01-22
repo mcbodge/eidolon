2f925f4d59ee1096685d5792c4c7c95a93752ce5
mcbodge <manuel.grillo@mail.polimi.it>
2016-01-22 23:42:48 +0100

    First structure of the tutorial.

---
+++ polimi.vpd.eidolon/Assets/Scripts/TutorialCanvasControl.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class TutorialCanvasControl : MonoBehaviour
+{
+    public List<Sprite> FirstSlot;
+    public List<Sprite> SecondSlot;
+    public List<Sprite> ThirdSlot;
+    public List<Sprite> FourthSlot;
+
+    private Image imageBox;
+    private KeyCode keyCodeToNextPicture;
+
+    private bool isManagerBusy;
+
+    void Start()
+    {
+        //isFirstSlotEnabled = isSecondSlotEnabled = isThirdSlotEnabled = isFourthSlotEnabled = false;
+        imageBox = gameObject.GetComponentInChildren<Image>();
+        isManagerBusy = false;
+        EnableSlot(FirstSlot);
+    }
+
+    private void EnableSlot(List<Sprite> specificSlot)
+    {
+        AC.KickStarter.cursorManager.cursorDisplay = AC.CursorDisplay.Never;
+        if (!isManagerBusy)
+            StartCoroutine(ManageSlot(specificSlot));
+#if UNITY_EDITOR
+        else
+            Debug.LogWarning("Canvas manager is busy!");
+#endif
+    }
+
+    private IEnumerator ManageSlot(List<Sprite> currentSlot)
+    {
+        imageBox.enabled = true;
+        imageBox.sprite = currentSlot[0];
+        int currentIndex = 1;
+        while (currentIndex <= currentSlot.Count)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                if (currentIndex < currentSlot.Count)
+                    imageBox.sprite = currentSlot[currentIndex];
+                else
+                    imageBox.enabled = false;
+                currentIndex++;
+            }
+            yield return null;
+        }
+        AC.KickStarter.cursorManager.cursorDisplay = AC.CursorDisplay.Always;
+    }
+}
