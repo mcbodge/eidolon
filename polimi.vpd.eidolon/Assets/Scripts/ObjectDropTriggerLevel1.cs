5b69e2f4d2027f27d0d2f18861b2a64e64411c89
Domenico Iezzi <domenico.iezzi.201@gmail.com>
2016-01-25 21:27:57 +0100

    Small fixes to level1

--- polimi.vpd.eidolon/Assets/Scripts/ObjectDropTriggerLevel1.cs
+++ polimi.vpd.eidolon/Assets/Scripts/ObjectDropTriggerLevel1.cs
@@ -12,6 +12,7 @@ public class ObjectDropTriggerLevel1 : MonoBehaviour {
 	
 	void OnTriggerEnter (Collider other)
     {
+        Debug.Log("ASDASDASD " + other.name);
         if (CheckEnteredObject(other.gameObject.name))
         {
             if (!actionHelper.PlacedObjects.Contains(other.gameObject))
@@ -22,9 +23,9 @@ public class ObjectDropTriggerLevel1 : MonoBehaviour {
             }
         }
 
-        if (other.gameObject.name.Equals("Christofer"))
+        if (other.gameObject.name.Equals("Christopher"))
         {
-            if (actionHelper.PlacedObjects.Count > 3)
+            if (actionHelper.PlacedObjects.Count >= 3)
             {
                 actionHelper.LoadSecondScene();
             }
