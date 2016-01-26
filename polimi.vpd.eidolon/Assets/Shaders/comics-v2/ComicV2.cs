1fde6e8e034b1366331a737d4c0d9b26680718eb
Daniele <danielee.gamba@gmail.com>
2016-01-26 18:23:10 +0100

    edited comicv2 to add an exception

--- polimi.vpd.eidolon/Assets/Shaders/comics-v2/ComicV2.cs
+++ polimi.vpd.eidolon/Assets/Shaders/comics-v2/ComicV2.cs
@@ -30,7 +30,7 @@ public class ComicV2 : MonoBehaviour
                 Material[] allMaterials = objectRenderer.materials;
                 foreach (Material mat in allMaterials)
                 {
-                    if(mat.name != "Particle")
+					if(mat.name != "Particle" && ! mat.name.Contains("ignore-color"))
                         mat.shader = Normals; // a material executes all the passes in the shader
                 }
                     
